dfd165b6dc6c35725d72b315249d6b8f5bbe613d
David Bureš <buresdv@gmail.com>
2024-10-11 21:53:42 +0200

    Initial Commit

---
+++ Navigation Problems/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navigation Problems
+//
+//  Created by David Bureš on 11.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation Problems/Navigation_ProblemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_ProblemsApp.swift
+//  Navigation Problems
+//
+//  Created by David Bureš on 11.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_ProblemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
